29c5ef6fe3a8ab9368394666d23411ae955c6120
anthonylamothe <anthony@molsoft.io>
2022-08-31 18:54:43 -0400

    Adding first files

---
+++ pages/index.php
@@ -0,0 +1,27 @@
+<?php session_start(); ?>
+
+<head>
+    <title>Acceuil Foodbook</title>
+    <meta charset="utf-8">
+    <?php require_once('fonctions.php'); ?>
+    <?php require_once('sql.php'); ?>
+    <style>
+        <?php require 'styles/index.css'; ?>
+    </style>
+</head>
+
+<?php 
+    // close session
+    // if(!empty($_SESSION))
+    // {
+    //     session_unset();
+    //     session_destroy();
+    // } 
+?>
+
+<body> 
+    <div class="wrapper">
+        Hello i am a wrapper
+    </div>  
+</body>
+
