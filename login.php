a106ebcbec79a9d4ecdd46d25e8623b7baa559a8
anthonylamothe <anthony@molsoft.io>
2022-09-01 09:33:28 -0400

    Adding button-tertiary, login page bones and moving banner style in must-have

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 <?php session_start(); ?>
 
 <head>
-    <title>Accueil Foodbook</title>
+    <title>Login Foodbook</title>
     <meta charset="utf-8">
     <style>
-        <?php require 'styles/index.css'; ?>
+        <?php require 'styles/login.css'; ?>
 
         <?php require 'styles/must-have.css'; ?>
         <?php require 'scripts/body-scripts.php'; ?>
@@ -23,12 +23,12 @@
 <body> 
     <div class="header-banner">
         <a href="index.php"><?php echo file_get_contents("utilities/foodbook-logo.svg"); ?></a>
-        <div class="banner-title"> Accueil </div>
-        <a href="login.php" class="button button-secondary login-button"> Login </a>
+        <div class="banner-title"> Login </div>
     </div>
+
     <div class="wrapper">
-        <a href="ui-kit.php" class="button button-primary">Accéder au ui-kit</a>
-    </div>  
+        
+    </div>
 
     <?php GenerateFooter(); ?>
 </body>
