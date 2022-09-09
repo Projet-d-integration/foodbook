80533fc99343ca43b94f3f47aa47587b4952f7a9
Zetryx97 <gablessard17@gmail.com>
2022-09-09 19:42:11 -0400

    Co-authored-by: AnthonyLamothe <AnthonyLamothe@users.noreply.github.com>
    change made

---
+++ recipes-list.php
@@ -0,0 +1,68 @@
+<?php session_start(); ?>
+
+<head>
+    <title>Recettes Foodbook</title>
+    
+    <meta charset="utf-8">
+    
+    <style>
+        <?php require 'styles/recipes-list.css'; ?>
+
+        <?php require 'styles/must-have.css'; ?>
+        <?php require 'scripts/body-scripts.php'; ?>
+    </style>
+    
+    <?php RenderFavicon(); ?>
+</head>
+
+<body>
+    <h1> Liste de recettes possible avec : <!--Poulet, boeuf,whatever--></h1>
+
+    <div class="recipes-container">
+        <div class="recipe-box">
+            recette
+        </div>
+        <div class="recipe-box">
+            recette
+        </div>
+        <div class="recipe-box">
+            recette
+        </div>
+        <div class="recipe-box">
+            recette
+        </div>
+        <div class="recipe-box">
+            recette
+        </div>
+        <div class="recipe-box">
+            recette
+        </div>
+        <div class="recipe-box">
+            recette
+        </div>
+        <div class="recipe-box">
+            recette
+        </div>
+        <div class="recipe-box">
+            recette
+        </div>
+        <div class="recipe-box">
+            recette
+        </div>
+        <div class="recipe-box">
+            recette
+        </div>
+        <div class="recipe-box">
+            recette
+        </div>
+        <div class="recipe-box">
+            recette
+        </div>
+        <div class="recipe-box">
+            recette
+        </div>
+        <div class="recipe-box">
+            recette
+        </div>
+    </div>
+</body>
