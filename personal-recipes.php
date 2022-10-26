f42f5ab17c0a5c6706e569971ea6d827a33b6d8a
anthonylamothe <anthony@molsoft.io>
2022-10-26 01:19:35 -0400

    switching computer

--- personal-recipes.php
+++ personal-recipes.php
@@ -58,8 +58,46 @@
 
             <div class="recipe-creation-form"></div>
         </div>
-    </div>
 
+        <div class="personal-recipes-add-recipe-form" id="personal-recipes-add-recipe-form">
+            <div class="transparent-background">
+                <form method="post" class="form-content">
+                    <div class="form-content-wrapper">
+                        <div class="form-exit" onclick='HideFormRecipeCreation()'> <?php echo file_get_contents("utilities/x-symbol.svg"); ?> </div>
+                        <div class="recipe-form-info"></div>
+    
+                        <div class="recipe-form-image-input">
+                            <label for="image-input">Image URL</label>
+                            <input type="text" name="image-input" class="text-input">
+                        </div>
+    
+                        <div class="recipe-form-title-input">
+                            <label for="title-input">Title</label>
+                            <input type="text" name="title-input" class="text-input">
+                        </div>
+
+                        <div class="recipe-form-ingredients">
+                           
+
+                            <?php
+                                // $nbIngredients <= 0
+                                if (true) {
+                                    echo '<div class="neutral_message" style="display: flex"> Vous n\'avez présentement aucun ingrédient dans votre recette </div>';
+                                }
+
+                                // add all ingredients already added here
+                            ?>
+                            
+                            
+
+                            <div class="button button-primary add-new-ingredient" id="add_new_ingredient" onclick='ShowFormAddNewIngredient()'>Ajouter un ingrédient</div>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
     <?php GenerateFooter(); ?>
 </body>
 
@@ -74,18 +112,26 @@ window.onload = () => {
             // $tabRecettes = ShowRecipe($_SESSION['idUser']);
             // $numTabRecette = count($tabRecettes);
             // $tabRecettes <= 0
-            if(-1 <= 0) {
+            if(true) {
                 echo 'document.getElementById("error_no_recipes").style.display = "block";';
             }
         }
     ?>
+}
 
     function ShowFormRecipeCreation() {
-        document.getElementById("recipe-creation-form").style.display = "block";
+        document.getElementById("personal-recipes-add-recipe-form").style.display = "block";
     }
 
     function HideFormRecipeCreation() {
-        document.getElementById("recipe-creation-form").style.display = "none";
+        document.getElementById("personal-recipes-add-recipe-form").style.display = "none";
     }
-}
+
+    function ShowFormAddNewIngredient() {
+        document.getElementById("personal-recipes-add-ingredient-form").style.display = "block";
+    }
+
+    function HideFormAddNewIngredient() {
+        document.getElementById("personal-recipes-add-ingredient-form").style.display = "none";
+    }
 </script>
