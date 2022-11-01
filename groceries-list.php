ca11db6568f8de773e437c90412c864850f6ecf1
Zetryx97 <gablessard17@gmail.com>
2022-11-01 16:05:07 -0400

    yeah

--- groceries-list.php
+++ groceries-list.php
@@ -12,7 +12,7 @@
 ?>
 
 <head>
-    <title>Liste d'épicerie youpie</title>
+    <title>Liste d'épicerie</title>
     <meta charset="utf-8">
     <style>
         <?php require 'styles/groceries-list.css'; ?>
