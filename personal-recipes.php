4e04f9a5c0d28077ef3d3ef3377058e63e689724
anthonylamothe <anthony@molsoft.io>
2022-10-31 22:03:06 -0400

    Commit des modifications pour ajouter les items

--- personal-recipes.php
+++ personal-recipes.php
@@ -67,7 +67,7 @@
                         <div class="recipe-form-info"></div>
     
                         <div class="recipe-form-image-input">
-                            <label for="image-input">Image URL</label>
+                            <label for="image-input">Url de l'image</label>
                             <input type="text" name="image-input" class="text-input">
                         </div>
     
@@ -77,26 +77,45 @@
                         </div>
 
                         <div class="recipe-form-ingredients">
-                           
-
                             <?php
                                 // $nbIngredients <= 0
                                 if (true) {
                                     echo '<div class="neutral_message" style="display: flex"> Vous n\'avez présentement aucun ingrédient dans votre recette </div>';
                                 }
-
+                                
                                 // add all ingredients already added here
                             ?>
-                            
-                            
-
                             <div class="button button-primary add-new-ingredient" id="add_new_ingredient" onclick='ShowFormAddNewIngredient()'>Ajouter un ingrédient</div>
+                        </div>
+                        
+                        <div class="recipe-form-steps">
+                            <?php 
+                                // $nbSteps <= 0
+                                if (true) {
+                                    echo '<div class="neutral_message" style="display: flex"> Vous n\'avez présentement aucun ingrédient dans votre recette </div>';
+                                }
+                                
+                                // add all ingredients already added here
+                            ?>
+                            <div class="button button-primary add-new-step" id="add_new_step" onclick='ShowFormAddNewStep()'>Ajouter une étape</div>
                         </div>
                     </div>
 
                 </form>
             </div>
         </div>
+
+        <div class="personal-recipes-add-recipe-form" id="personal-recipes-add-ingredient-form">
+            <div class="transparent-background">
+                <form method="post" class="items-form-content">
+                    <div class="form-content-wrapper">
+                        <div class="form-exit" onclick='HideFormAddNewIngredient()'> <?php echo file_get_contents("utilities/x-symbol.svg"); ?> </div>
+                        <!-- add items form here -->
+                    </div>
+                </form>
+            </div>
+        </div>
+            
     </div>
     <?php GenerateFooter(); ?>
 </body>
@@ -133,5 +152,13 @@ window.onload = () => {
 
     function HideFormAddNewIngredient() {
         document.getElementById("personal-recipes-add-ingredient-form").style.display = "none";
+    }
+
+    function ShowFormAddNewStep() {
+        document.getElementById("personal-recipes-add-step-form").style.display = "block";
+    }
+
+    function HideFormAddNewStep() {
+        document.getElementById("personal-recipes-add-step-form").style.display = "none";
     }
 </script>
