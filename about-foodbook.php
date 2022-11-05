1254290042dbbe0fa3822ad7881fc21c5a0a215c
Zetryx97 <gablessard17@gmail.com>
2022-11-05 16:17:34 -0400

    about page done for now

--- about-foodbook.php
+++ about-foodbook.php
@@ -41,33 +41,33 @@
 
 
         <div class="main-wrapper">
-            <div class="container-main-display">
+            <div class="wrapper-about">
                 
-            <div class="image-about-page">
-            <img src="https://img.freepik.com/vecteurs-libre/equipe-commerciale-mettant-ensemble-jigsaw-puzzle-isole-illustration-vectorielle-plane-partenaires-dessin-anime-travaillant-connexion-concept-travail-equipe-partenariat-cooperation_74855-9814.jpg">
-            </div>
-                    <div class="message-about-us">
-                        <h2>Qui sommes-nous ?</h2>
-                        <p>
-                        Notre site FoodBook réalisé par quatres jeunes étudiants travaillants et inventifs du Collège Lionel-Groulx est à la fois 
-                        un gestionnaire de nourriture et de recettes ainsi qu'un réseau social hors du commun. Il est à seul but de faciliter la vie des
-                        gens lorsqu'il est question d'alimentation car nous savons de nos jours que c'est un sujet qui peut être compliqué.
-                        </p>
-                        <p>
-                        Pour ce qui est du réseau social, nous voulons que nos utilisateurs se sentent à l'aisent de publier, commenter et évaluer
-                        les recettes des autres utilisateurs dans le plus grand respect. Nous voulons que cela puissent également créer des amitiés entre les utilisateurs
-                        afin que notre site soit propice à un environnement agréable pour tous.    
-                        </p>
+                <div class="image-about-page">
+                <img src="https://img.freepik.com/vecteurs-libre/equipe-commerciale-mettant-ensemble-jigsaw-puzzle-isole-illustration-vectorielle-plane-partenaires-dessin-anime-travaillant-connexion-concept-travail-equipe-partenariat-cooperation_74855-9814.jpg">
+                </div>
+                        <div class="message-about-us">
+                            <h2>Qui sommes-nous ?</h2>
+                            <p>
+                            Notre site FoodBook réalisé par quatres jeunes étudiants travaillants et inventifs du Collège Lionel-Groulx est à la fois 
+                            un gestionnaire de nourriture et de recettes ainsi qu'un réseau social hors du commun. Il est à seul but de faciliter la vie des
+                            gens lorsqu'il est question d'alimentation car nous savons de nos jours que c'est un sujet qui peut être compliqué.
+                            </p>
+                            <p>
+                            Pour ce qui est du réseau social, nous voulons que nos utilisateurs se sentent à l'aisent de publier, commenter et évaluer
+                            les recettes des autres utilisateurs dans le plus grand respect. Nous voulons que cela puissent également créer des amitiés entre les utilisateurs
+                            afin que notre site soit propice à un environnement agréable pour tous.    
+                            </p>
 
-                        <p>
-                           Comme mentionné plutôt, nous sommes quatres étudiants du Collège Lionel-Groulx  qui se sont données coprs et âmes à la confection
-                           de FoodBook. Étant tous jeunes et passionés d'informatique nous avons fait tout ce qui étaient en notre pouvoir grâce l'aide de nos connaissances et aptitudes, nous voulions simplement facilité et amélioré la vie des gens qui auraient de 
-                           la difficulté à gérer leur nourriture ainsi que pouvoir créer des liens d'amitiés sain et affectueux. Nous vous remercions infiniement de faire confiance à notre site web
-                           et espèrons que vous allez prendre plaisir à toutes ses fonctionnalités.
-                        </p>
-                    </div>
-                    
-            </div>
+                            <p>
+                            Comme mentionné plutôt, nous sommes quatres étudiants du Collège Lionel-Groulx  qui se sont données coprs et âmes à la confection
+                            de FoodBook. Étant tous jeunes et passionés d'informatique nous avons fait tout ce qui étaient en notre pouvoir grâce l'aide de nos connaissances et aptitudes, nous voulions simplement facilité et amélioré la vie des gens qui auraient de 
+                            la difficulté à gérer leur nourriture ainsi que pouvoir créer des liens d'amitiés sain et affectueux. Nous vous remercions infiniement de faire confiance à notre site web
+                            et espèrons que vous allez prendre plaisir à toutes ses fonctionnalités.
+                            </p>
+                        </div>
+                        
+                </div>
             
     
         </div>
