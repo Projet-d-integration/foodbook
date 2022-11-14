3f916b2952fbee3c425eddea1e4baa9076f30ef9
Zetryx97 <gablessard17@gmail.com>
2022-11-14 10:25:29 -0500

    starting working on mobile inventory

--- inventory.php
+++ inventory.php
@@ -30,9 +30,8 @@
         if($_SERVER['REQUEST_METHOD'] == 'POST')
             AddAnimation();
     ?>
-    <div class="header-banner">
+    <div class="header-banner hide-mobile">
         <a href="index.php"><?php echo file_get_contents("utilities/foodbook-logo.svg"); ?></a>
-        <div class="banner-title"> Inventaire </div>
         <?php AddSearchBar(); ?>
         <div class="svg-wrapper">
             <a href="personal-recipes.php" class="svg-button list-button"> <?php echo file_get_contents("utilities/book.svg"); ?> </a>
@@ -41,14 +40,60 @@
             <?php 
                 if(!empty($_SESSION['idUser'])){
                     echo '<a href="edit-profil.php" class="svg-button login-button"> '.file_get_contents("utilities/account.svg").'</a>';
-                    echo '<form method="post"><button type="submit" name="buttonDeconnecter" class="svg-button login-button" value="buttonDeconnecter" />'.file_get_contents("utilities/logout.svg").'</form>';
+                    echo '<form method="post"><button type="submit" name="buttonDeconnecter" class="svg-button login-button logout-button" value="buttonDeconnecter" />'.file_get_contents("utilities/logout.svg").'</form>';
                 }
                 else{
                     echo '<a href="login.php" class="svg-button login-button"> '.file_get_contents("utilities/account.svg").'</a>';
                 }
             ?>
         </div>
-    </div> 
+    </div>
+
+     <!-- mobile header -->
+    <div class="header-mobile-banner hide-desktop">
+        <a href="index.php"><?php echo file_get_contents("utilities/foodbook-logo.svg"); ?></a>
+        <button class="menu-icon" onclick="ShowMenu()"><?php echo file_get_contents("utilities/menu.svg"); ?></button>
+    </div>
+
+    <div class="mobile-popup-menu hide-desktop" id="mobile-popup-menu">
+        <div class="mobile-svg-wrapper hide-desktop">
+            <?php AddSearchBar(); ?>
+
+            <a href="personal-recipes.php" class="svg-button list-button"> 
+                <?php echo file_get_contents("utilities/book.svg"); ?> 
+                <span class="header-text">Vos recettes</span>
+            </a>
+            <a href="groceries-list.php" class="svg-button list-button"> 
+                <?php echo file_get_contents("utilities/list.svg"); ?> 
+                <span class="header-text">Liste d'épicerie</span>
+            </a>
+            <a href="inventory.php" class="svg-button inventory-button"> 
+                <?php echo file_get_contents("utilities/food.svg"); ?> 
+                <span class="header-text">Inventaire</span>
+            </a>
+            <div class="form-exit" onclick='HideMenu()'> <?php echo file_get_contents("utilities/x-symbol.svg"); ?> </div>
+            <?php 
+                if(!empty($_SESSION['idUser'])){
+                    echo '<a href="edit-profil.php" class="svg-button login-button"> 
+                        '.file_get_contents("utilities/account.svg");
+                        echo "
+                        <span class='header-text'>" . User($_SESSION['idUser'])[2] . " " . User($_SESSION['idUser'])[1] . "</span>
+                    </a>";
+                    echo '<form method="post">
+                    <button type="submit" name="buttonDeconnecter" class="svg-button login-button logout-button" value="buttonDeconnecter" />
+                        '.file_get_contents("utilities/logout.svg").'
+                        <span class="header-text">Se déconnecter</span>
+                    </form>';
+                }
+                else{
+                    echo '<a href="login.php" class="svg-button login-button logout-button"> 
+                    '.file_get_contents("utilities/account.svg").'
+                    <span class="header-text">Se connecter</span>
+                    </a>';
+                }
+            ?>
+        </div>
+    </div>
 
         <div class="wrapper">
             <?php 
@@ -257,5 +302,12 @@
 
     function HideFormItemQuantity(id) {
         document.getElementById("inventory-item-form-" + id).style.display = "none";
+    }
+    function ShowMenu() {
+        document.getElementById("mobile-popup-menu").style.display = "flex";
+    }
+
+    function HideMenu() {
+        document.getElementById("mobile-popup-menu").style.display = "none";
     }
 </script>
